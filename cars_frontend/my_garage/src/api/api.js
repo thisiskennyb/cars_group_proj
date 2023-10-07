d7e8dceee5f5dd6820c316942a8de362a1c17e17
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2023-10-07 11:27:00 -0400

    created production branch

--- cars_frontend/my_garage/src/api/api.js
+++ cars_frontend/my_garage/src/api/api.js
@@ -13,7 +13,8 @@ async function basicFetch(url, payload) {
         },
         body: JSON.stringify(context)
       }
-      const body = await basicFetch("http://localhost:8000/accounts/signup",payload)
+      const base_url = import.meta.env.VITE_BASE_URL
+      const body = await basicFetch(`http://${base_url}/accounts/signup`,payload)
       console.log(body, "API CALL")
       return body
     }
@@ -27,7 +28,8 @@ async function basicFetch(url, payload) {
       },
       body: JSON.stringify(context)
     }
-    const body = await basicFetch("http://localhost:8000/accounts/get-token", payload)
+    const base_url = import.meta.env.VITE_BASE_URL
+    const body = await basicFetch(`http://${base_url}/accounts/get-token`, payload)
     localStorage.setItem("token", body.token)
     return body.token
   }
@@ -45,7 +47,8 @@ async function basicFetch(url, payload) {
       body: JSON.stringify(context)
       
     }
-    const body = await basicFetch("http://localhost:8000/mygarage/", payload)
+    const base_url = import.meta.env.VITE_BASE_URL
+    const body = await basicFetch(`http://${base_url}/mygarage/`, payload)
     console.log(body)
     return {"congrats": "on your new car"}
   }
@@ -61,7 +64,8 @@ async function basicFetch(url, payload) {
         "Authorization": `Token ${userToken}`
       },
     }
-    const body = await basicFetch("http://localhost:8000/mygarage/", payload)
+    const base_url = import.meta.env.VITE_BASE_URL
+    const body = await basicFetch(`http://${base_url}/mygarage/`, payload)
     // const results = await body.json()
 
     return body
