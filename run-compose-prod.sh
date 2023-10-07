b7771a3c22a47248bcca71adf9dce87ffa7fe941
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2023-10-07 15:20:36 -0400

    more changes

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -17,5 +17,5 @@ docker-compose -f docker-compose.prod.yml up -d
 
 # make sure the postgres container is ready, then run migrations
 sleep 10 
-docker exec ec2-user-api-1 python /src/manage.py makemigrations 
-docker exec ec2-user-api-1 python /src/manage.py migrate
+docker exec cars_group_proj-api-1 python /src/manage.py makemigrations 
+docker exec cars_group_proj-api-1 python /src/manage.py migrate
