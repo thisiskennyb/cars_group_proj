0cfce534a90767f25bb317410778d0cf693c0081
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2023-10-07 11:43:56 -0400

    more production config

--- cars_backend/cars_proj/urls.py
+++ cars_backend/cars_proj/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('accounts/', include("accounts.urls")),
-    path('admin/', admin.site.urls),
-    path('car/', include('car_app.urls')),
-    path('mygarage/', include('my_garage.urls'))
+    path('api/accounts/', include("accounts.urls")),
+    path('api/admin/', admin.site.urls),
+    path('api/car/', include('car_app.urls')),
+    path('api/mygarage/', include('my_garage.urls'))
 ]
